764c95f3cf109cfb237819983d3493dfce2b90e3
Cameron Gorrie <sastraxi@gmail.com>
2020-10-11 12:51:16 -0400

    add a robot running in place

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
 use bevy::prelude::*;
 
-struct Person;
-
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn((Person, Name("Elaina Proctor".to_string())))
-        .spawn((Person, Name("Renzo Hume".to_string())))
-        .spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, mut query: Query<(&Person, &Name)>) {
-    timer.0.tick(time.delta_seconds);
-    if timer.0.finished {
-        for (_person, name) in &mut query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
+mod sprite;
 
 fn main() {
     App::build()
+        .add_resource(WindowDescriptor {
+            title: "hyperbevy".to_string(),
+            width: 1280,
+            height: 720,
+            vsync: false,
+            resizable: false,
+            ..Default::default()
+        })
         .add_default_plugins()
-        .add_plugin(HelloPlugin)
+        .add_startup_system(sprite::setup.system())
+        .add_system(sprite::animate.system())
         .run();
 }
---
+++ src/sprite.rs
@@ -0,0 +1,37 @@
+use bevy::prelude::*;
+
+const SPRITE_SCALE: f32 = 0.5;
+
+pub fn animate(
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (timer, mut sprite, texture_atlas_handle) in &mut query.iter() {
+        if timer.finished {
+            let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
+            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
+}
+
+pub fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut textures: ResMut<Assets<Texture>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server
+        .load_sync(&mut textures, "assets/character_robot_sheet.png")
+        .unwrap();
+    let texture = textures.get(&texture_handle).unwrap();
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 9, 5);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands
+        .spawn(Camera2dComponents::default())
+        .spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(SPRITE_SCALE),
+            ..Default::default()
+        })
+        .with(Timer::from_seconds(0.1, true));
+}
